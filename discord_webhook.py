6b0924b143ff623b2aa33bc639a287601cc34077
kakimind100 <kakimind100@gmail.com>
2024-11-17 10:02:55 -0500

    Update discord_webhook.py

--- discord_webhook.py
+++ discord_webhook.py
@@ -22,7 +22,7 @@ def send_to_discord_webhook(webhook_url, message):
 
 # AI를 사용하여 주식 분석 결과를 생성하는 함수
 def generate_ai_response(stock_data):
-    prompt = "주어진 주식 데이터를 기반으로 다음 거래일에 가장 많이 오를 종목을 3개 추천해 주세요. 결과는 종목명만 적어 주세요"
+    prompt = "주어진 주식 데이터를 기반으로 다음 거래일에 가장 많이 오를 종목을 3개 추천해 주세요. 결과는 검색된 종목수와 종목명만 적어 주세요"
     
     for stock in stock_data:
         prompt += (f"종목 코드: {stock['Code']}, "
