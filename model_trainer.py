d1d86e9443d4cf87dcc95fdf1430f6f59d647559
kakimind100 <kakimind100@gmail.com>
2024-12-02 07:11:35 -0500

    Update model_trainer.py

--- model_trainer.py
+++ model_trainer.py
@@ -89,7 +89,7 @@ def prepare_data(df):
             high_price = stock_data['High'].max()
 
             # 타겟 설정: 오늘 최저가에서 최고가가 20% 이상 상승했는지 여부
-            target_today = 1 if high_price > low_price * 1.2 else 0
+            target_today = 1 if high_price > low_price * 1.29 else 0
 
             # 마지막 날의 피처와 타겟을 함께 추가
             X.append(stock_data[features].values[-1])  # 마지막 날의 피처 사용
