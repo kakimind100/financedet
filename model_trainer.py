41d2a1f953034abd54b936e958ce885a7037a6f5
kakimind100 <kakimind100@gmail.com>
2024-12-07 06:43:08 -0500

    Update model_trainer.py

--- model_trainer.py
+++ model_trainer.py
@@ -49,9 +49,9 @@ def optimize_hyperparameters_bayes(X_train, y_train):
         return mse
 
     param_bounds = {
-        'n_estimators': (60, 70),  # 범위 축소
+        'n_estimators': (50, 100),  # 범위 축소
         'learning_rate': (0.05, 0.2),
-        'max_depth': (3, 8),
+        'max_depth': (3, 6),
         'subsample': (0.7, 1),
         'colsample_bytree': (0.5, 1),
         'alpha': (0, 10),
